584975e15994c76921e7d5650e5526667b5b5828
pierreiost <pierreiost8@gmail.com>
2025-11-11 21:56:33 -0300

    fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  headers: {
+ baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api', 
+   headers: {
     'Content-Type': 'application/json',
   },
 });
